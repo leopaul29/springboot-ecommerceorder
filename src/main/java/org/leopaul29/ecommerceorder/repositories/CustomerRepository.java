6325016a2773d5b2a1f692f4f4303a0f5baffe3a
Leo-paul <leopaulmartin@hotmail.fr>
2023-04-16 19:28:39 +0200

    add repository

--- src/main/java/org/leopaul29/ecommerceorder/repositories/CustomerRepository.java
+++ src/main/java/org/leopaul29/ecommerceorder/repositories/CustomerRepository.java
@@ -2,11 +2,13 @@ package org.leopaul29.ecommerceorder.repositories;
 
 import org.leopaul29.ecommerceorder.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
+@Component
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Optional<Customer> findByName(String name);
 }
--- src/test/java/org/leopaul29/ecommerceorder/repositories/CustomerRepositoryTest.java
+++ src/test/java/org/leopaul29/ecommerceorder/repositories/CustomerRepositoryTest.java
@@ -1,22 +1,45 @@
 package org.leopaul29.ecommerceorder.repositories;
 
-import jakarta.annotation.Resource;
+import jakarta.persistence.EntityManager;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.leopaul29.ecommerceorder.entities.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.sql.DataSource;
+
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 public class CustomerRepositoryTest {
-    @Resource
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private EntityManager entityManager;
+    @Autowired
     private CustomerRepository customerRepository;
+    private Customer customer;
 
+    @BeforeEach
+    public void setup() {
+        customer = Customer.builder().name("Alex").build();
+    }
+    
     @Test
     public void givenCustomer_whenSave_thenSavedCustomer() {
-        Customer customer = Customer.builder().name("Alex").build();
+        // given - setup or precondition
+        Customer customer = Customer.builder().name("Bernard").build();
 
+        // when - action or the testing
         Customer savedCustomer = customerRepository.save(customer);
 
+        // then - very output
         Assertions.assertNotNull(savedCustomer);
         Assertions.assertNotNull(savedCustomer.getId());
     }
